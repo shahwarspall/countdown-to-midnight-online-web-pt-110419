8e8c157bdef9b47c1055f7360c91833e0f78485f
Shahwar Spall <shevy.spall@gmail.com>
2019-11-07 21:29:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,8 +10,8 @@ return "HAPPY NEW YEAR!"
 end
 
 
-def countdown_with_sleep
-    number=10
+def countdown_with_sleep(number)
+  number=10
   while number > 0 
   puts "#{number} SECOND(S)!"
     number -= 1
